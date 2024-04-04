040f8d06ef18e59e6e70effe84ca5aa1389d3766
azrulAmat <165981885+azrulAmat@users.noreply.github.com>
2024-04-04 12:25:12 +0800

    Add files via upload

---
+++ PushButton_Debounce/PushButton_Debounce.ino
@@ -0,0 +1,32 @@
+#define SW1 8
+#define dbounceDelay 40
+
+unsigned long lowcntr;
+unsigned char sw1_stat;
+
+void setup() {
+  pinMode(SW1,INPUT_PULLUP);
+  Serial.begin(38400);
+  lowcntr=0;
+  sw1_stat=0;
+}
+
+void loop() {
+
+  int sw1 = digitalRead(SW1);
+
+  if(sw1==0){
+    if(sw1_stat==0){
+      lowcntr=millis();
+      sw1_stat=1;
+    }
+    else if(millis()-lowcntr > dbounceDelay && sw1_stat != 2){
+      Serial.println("SW-Pressed");
+      sw1_stat=2;
+    }
+  }
+  else{
+    sw1_stat=0;
+  }
+  
+}
